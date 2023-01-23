6a72cf30c38e6d283c73d223c2e3a21e02693d1e
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-01-23 20:40:54 +0300

    Example 3 done

---
+++ ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите число и узнайте четное или нечетное: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ответ");
+if (numberA % 2 == 0)
+{
+System.Console.WriteLine("Четное");
+}
+else
+System.Console.WriteLine("Нечетное");
