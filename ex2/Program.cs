3308ef50ac19bb92b8e5dd8c575789912a4a18b9
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-01-23 19:24:17 +0300

    Example 2 done

---
+++ ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите два числа и большее Вы увидете на экране");
+System.Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Большее число: ");
+
+if (numberA > numberB)
+{
+    if (numberA>numberC)
+    {
+        System.Console.WriteLine(numberA);
+    }
+    else
+    {
+        System.Console.WriteLine(numberC);
+    }
+}
+else
+{
+    if (numberB > numberC)
+    {
+        System.Console.WriteLine(numberB);
+    }
+    else
+    {
+        System.Console.WriteLine(numberC);
+    }
+}
